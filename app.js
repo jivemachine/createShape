6c59f549926c65913f57bac41590b675bea1a5d3
Jeremy <the.jeremy.cobb@gmail.com>
2023-08-14 19:30:16 -0500

    initalized and started coding classes for project

---
+++ app.js
@@ -0,0 +1,30 @@
+class Shape {
+    constructor() {
+
+    };
+
+};
+
+class Circle {
+    constructor(params) {
+
+    };
+};
+
+class Triangle {
+    constructor(params) {
+        
+    };
+};
+
+class Rectangle {
+    constructor(params) {
+        
+    };
+};
+
+class Square {
+    constructor(params) {
+        
+    };
+};
