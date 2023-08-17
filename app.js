0f457107a24e6cff54ff70fa2386a352b25b1589
Jeremy <the.jeremy.cobb@gmail.com>
2023-08-17 13:02:19 -0500

    added triangle class & functionality

--- app.js
+++ app.js
@@ -102,8 +102,8 @@ $('[id="CircleBtn"]').click(function () {
 class Rectangle extends Shape {
     constructor(height, width, cl, id) {
         super(cl, id);
-        this.height = height;
-        this.width = width;
+        this.height = height+'px';
+        this.width = width+'px';
     }
     draw() {
         this.createDiv('rectangleDiv', 'thisRectangle');
@@ -138,5 +138,48 @@ $('[id="RectangleBtn"]').click(function () {
     // dbl click shape to delete shape div
     $(document).on('dblclick', '.rectangleDiv', function() {
         $('.rectangleDiv').remove();
+    })
+});
+
+
+// triangle class (right isoceles triangle)
+class Triangle extends Shape {
+    constructor(height, cl, id) {
+        super(cl, id);
+        this.height = height+'px';
+    }
+    draw() {
+        this.createDiv('triangleDiv', 'thisTriangle');
+        $('.triangleDiv').css('width', `${this.height}`);
+        $('.triangleDiv').css('height', `${this.height}`);
+        $('.triangleDiv').css('border-bottom', `${this.height} solid green`);
+        $('.triangleDiv').css('border-right', `${this.height} solid transparent`);
+        let randY = Math.floor((Math.random() * 200) + 1);
+        let randX = Math.floor((Math.random() * 200) + 1);
+        $('.triangleDiv').css('transform', `translate(${randX}px, ${randY}px)`);
+    }
+    describe() {
+        $('.shapeName').text('Triangle');
+        $('.shapeWidth').text(`Width: ${this.height}`);
+        $('.shapeHeight').text(`Height: ${this.height}`);
+        $('.shapeRadius').text(`Radius: ${this.radius}`);
+        $('.shapePerimeter').text(`Perimeter: ${this.perimeter}`);
+    }
+}
+
+// triangle btn
+$('[id="IsocelesBtn"]').click(function () {
+    let height = $('[id="IsocelesHeight"]').val();
+    let tri = new Triangle(height);
+    tri.draw();
+
+    // click shape to describe
+    $(document).on('click', '.triangleDiv', function () {
+        tri.describe();
+    })
+
+    // dbl click shape to delete shape div
+    $(document).on('dblclick', '.triangleDiv', function() {
+        $('.triangleDiv').remove();
     })
 });
